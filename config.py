854a9f9065387181c27ffd901544681bdd7ea6eb
Anssi <kaneran21@hotmail.com>
2022-09-10 14:06:49 +0100

    Sync with submission template

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ import json
 # if you tinker with them, your submissions will almost certainly fail!
 
 # 10 for evaluation + 1 leaderboard video
-EVAL_EPISODES = 11
+EVAL_EPISODES = int(os.getenv("AICROWD_NUM_EVAL_EPISODES", 11))
 # This is only used to limit steps when debugging is on.
 # Environments will automatically return done=True once
 # the environment-specific timeout is reached
-EVAL_MAX_STEPS = int(1e9)
+EVAL_MAX_STEPS = int(os.getenv("AICROWD_NUM_EVAL_MAX_STEPS", 1e9))
 
 settings = json.load(open('aicrowd.json'))
 if settings.get('debug', False):
     # if debug flag is set to true, evaluation will only run a single episode for 100 steps.
     # Again, this will be enforced by the evaluation server, and do not do more episodes/steps
     # than this
-    EVAL_EPISODES = 2
-    EVAL_MAX_STEPS = 100
+    EVAL_EPISODES = int(os.getenv("AICROWD_NUM_EVAL_EPISODES", 2))
+    EVAL_MAX_STEPS = int(os.getenv("AICROWD_NUM_EVAL_MAX_STEPS", 100))
