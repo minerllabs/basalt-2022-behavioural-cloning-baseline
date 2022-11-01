074df2882cc379e3be38c66a0e20aaab119a4dad
MuhammadHakami <engmuhammadhakami@gmail.com>
2022-11-01 07:46:56 +0300

    update resume for downloading dataset

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -12,12 +12,34 @@
 import argparse
 import urllib.request
 import os
+import glob, os, cv2, json
 
 parser = argparse.ArgumentParser(description="Download OpenAI contractor datasets")
 parser.add_argument("--json-file", type=str, required=True, help="Path to the index .json file")
 parser.add_argument("--output-dir", type=str, required=True, help="Path to the output directory")
 parser.add_argument("--num-demos", type=int, default=None, help="Maximum number of demonstrations to download")
 
+def relpaths_to_download(relpaths, output_dir):
+    def read_json(file_name):
+        with open(file_name.replace('mp4', 'jsonl'), 'r') as json_file:
+            text = json.loads('['+''.join(json_file.readlines()).replace('\n', ',')+']')
+
+    data_path = '/'.join(relpaths[0].split('/')[:-1])
+    non_defect=[]
+    for vid_name in glob.glob(os.path.join(output_dir,'*.mp4')):
+        try:
+            vid = cv2.VideoCapture(vid_name)
+            read_json(vid_name.replace('mp4', 'jsonl'))
+            if vid.isOpened():
+                non_defect.append(os.path.join(data_path, vid_name.split('/')[-1]))
+        except:
+            continue
+
+    relpaths = set(relpaths)
+    non_defect = set(non_defect)
+    diff_to_download = relpaths.difference(non_defect)
+    print('total:', len(relpaths), '| exist:', len(non_defect), '| downloading:', len(diff_to_download))
+    return diff_to_download
 
 def main(args):
     with open(args.json_file, "r") as f:
@@ -30,6 +52,7 @@ def main(args):
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
+    relpaths = relpaths_to_download(relpaths, args.output_dir)
 
     for i, relpath in enumerate(relpaths):
         url = basedir + relpath
